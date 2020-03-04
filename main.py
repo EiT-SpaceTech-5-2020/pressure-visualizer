449b11ecb0314efc0ad881cb2ca5667ef886a888
Eivind Vold Aunebakk <eivind@aunebakk.no>
2020-03-04 13:28:11 +0100

    Reading from serial adapter

--- main.py
+++ main.py
@@ -3,6 +3,10 @@ from kivy.uix.screenmanager import ScreenManager,Screen
 from kivy.properties import ObjectProperty
 from kivy.uix.boxlayout import BoxLayout
 
+from SerialAdapter import SerialAdapter
+from Graph import Graph
+
+global sa
 
 class Menu(BoxLayout):
     manager = ObjectProperty(None)
@@ -28,10 +32,16 @@ class Manager(ScreenManager):
 
 class PressureVisualizer(App):
     def build(self):
+        sa = SerialAdapter()
+        sa.open('COM1')
+        sa.startReading()
         return Menu()
     def visualize(self):
         print("WOOO")
 
-
 if __name__ == '__main__':
     PressureVisualizer().run()
+
+#graph = Graph(sa)
+#Clock.schedule_interval(graph.update, 1.0 / 60.0)
+#self.add_widget(graph)
